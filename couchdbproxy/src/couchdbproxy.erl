82e89a07792881cb8756d0892f6d1c986014e8ec
Benoit Chesneau <bchesneau@gmail.com>
2009-10-26 15:12:33 +0100

    add credit

--- couchdbproxy/src/couchdbproxy.erl
+++ couchdbproxy/src/couchdbproxy.erl
@@ -10,6 +10,9 @@
 % WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 % License for the specific language governing permissions and limitations under
 % the License.
+% 
+% get_app_env function come from riak project under Apache License 2
+% Copyright 2007-2009 Basho Technologies
 
 %% @doc TEMPLATE.
 
--- couchdbproxy/src/couchdbproxy_app.erl
+++ couchdbproxy/src/couchdbproxy_app.erl
@@ -10,6 +10,10 @@
 % WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 % License for the specific language governing permissions and limitations under
 % the License.
+%
+% read_config, set-erlenv functions come from riak project under Apache License 2
+% Copyright 2007-2009 Basho Technologies
+%
 
 -module(couchdbproxy_app).
 -author('author <author@example.com>').
